42f2204c5d5789a46a64b6423573ce22977780ff
daveyhert <daveyhert@gmail.com>
2025-06-01 20:18:52 +0100

    refactor: remove unused components and assets to streamline the codebase

--- src/components/ui/OnboardingContent.tsx
+++
@@ -1,8 +0,0 @@
-interface OnboardingContentProps {
-  currentStep: number;
-  children: React.ReactNode;
-}
-
-export default function OnboardingContent({ children }: OnboardingContentProps) {
-  return <div className="onboarding-content">{children}</div>;
-}
